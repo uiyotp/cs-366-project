ccf79c17112bbf8d7c77c435608a0c48bc360b5f
uiyotp <solomonpaprocki@gmail.com>
2021-05-01 10:48:34 -0500

    Working on a running gameSalesAPI outline

--- game-sales/gameSalesAPI.java
+++ game-sales/gameSalesAPI.java
@@ -1,40 +1,95 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-public class gameSalesAPI {
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
- 	static final String databasePrefix ="cs366-2211_paprockisj04";
+public class gameSalesAPI extends HttpServlet
+{  
+	static final String databasePrefix ="cs366-2211_paprockisj04";
     static final String netID ="paprockisj04";
     static final String hostName ="washington.uww.edu";
     static final String databaseURL ="jdbc:mariadb://"+hostName+"/"+databasePrefix;
     static final String password="sp3619";
+     		    
+	private Connection connection = null;
+	private Statement statement = null;
+	private ResultSet resultSet = null;
 
-    private Connection connection = null;
-    private Statement statement = null;
-    private ResultSet resultSet = null;
-    
-    public void Connection(){
-  
-      try {
-            Class.forName("org.mariadb.jdbc.Driver");
-              System.out.println("databaseURL"+ databaseURL);
-            connection = DriverManager.getConnection(databaseURL, netID, password);
-            System.out.println("Successfully connected to the database");
-         }
-        catch (ClassNotFoundException e) {
-            e.printStackTrace();
+	public void Connection(){ 
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		    System.out.println("databaseURL"+ databaseURL);
+			connection = DriverManager.getConnection(databaseURL, netID, password);
+			System.out.println("Successfully connected to the database");
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+    //For all POST HTTP REQUESTS (Will not work for GETs)
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+	{
+        //Open the connection
+		gameSalesAPI gameObj=new gameSalesAPI();
+		gameObj.Connection();
+
+        //For the topGameFilter form post
+		if (request.getParameter("formName") == "topGameFilter"){
+            String filter = request.getParameter("topMetric");
+            String sql = "";			
+			if (filter == "critic") {
+				sql = 'SELECT g.name, p.name, d.name, g.year, g.genre, g.platform, g.critic_score, g.user_score, g.age_rating FROM game AS g
+                INNER JOIN publisher AS p
+                ON g.pub_ID = p.pub_ID
+                INNER JOIN developer AS d
+                ON g.dev_ID = d.dev_ID
+                WHERE g.critic_count > 10
+                ORDER BY g.critic_score desc
+                LIMIT 10;';	
+			}
+			if (filter == "user") {
+				sql = 'SELECT g.name, p.name, d.name, g.year, g.genre, g.platform, g.critic_score, g.user_score, g.age_rating FROM game AS g
+                INNER JOIN publisher AS p
+                ON g.pub_ID = p.pub_ID
+                INNER JOIN developer AS d
+                ON g.dev_ID = d.dev_ID
+                WHERE g.user_count > 10
+                ORDER BY g.user_score desc
+                LIMIT 10;';
+			}
+			if (filter == "sales") {
+				sql = 'SELECT g.name, p.name, d.name, g.year, g.genre, g.platform, g.critic_score, g.user_score, g.age_rating
+                FROM game g, publisher p, developer d, sales s,
+                WHERE g.game_ID in(
+                SELECT game_ID
+                FROM game g
+                WHERE game_ID in(
+                SELECT g.game_ID, (s.na_sales + s.eu_sales + s.jp_sales + s.other_sales ) AS total_sales
+                FROM game g, sales s
+                WHERE s.game_ID=g.game_ID
+                ORDER BY total_sales desc
+                LIMIT 10));';
+			}
+            gameObj.simpleQuery(sql);
+        }
+		
+        //Close the connection
+        try {
+            connection.close();
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
-    } // end of Connection
-    
-    public void simpleQuery(String sqlQuery) {
-    
+		
+	}
+		
+	public String simpleQuery(String sqlQuery) {
         try {
             statement = connection.createStatement();
             resultSet = statement.executeQuery(sqlQuery);
@@ -60,19 +115,41 @@ public class gameSalesAPI {
             e.printStackTrace();
         }
     }
-    
-    public static void main(String args[]) {
 
-        gameSalesAPI demoObj = new gameSalesAPI();
-        demoObj.Connection();
-        try {
-            connection.close();
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-        //String sqlQuery = "";
-        //demoObj.simpleQuery(sqlQuery);
-    }
+    public void storeProcedure(String procedure) {
+		try {
+			statement = connection.createStatement();
+			int total =0;
+			CallableStatement myCallStmt = connection.prepareCall("{call " + procedure + " (?)}");
+		    myCallStmt.registerOutParameter(1,Types.BIGINT);
+		    myCallStmt.execute();
+		    total = myCallStmt.getInt(1);
+		    System.out.println("The procedure " + procedure + " returned: "+ total);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	public void storeProcedure(String procedure, String input) {
+		try {
+			statement = connection.createStatement();
+			CallableStatement myCallStmt;
+			String result = "";
+			if(input == "") {
+				myCallStmt = connection.prepareCall("{call " + procedure + " (?)}");
+			}else {
+				myCallStmt = connection.prepareCall("{call " + procedure + " (?," + input + ")}");
+			}
+			myCallStmt.setString(1,"");
+		    myCallStmt.registerOutParameter(1,Types.VARCHAR);
+	        myCallStmt.execute();
+	        result = myCallStmt.getString(1);
+		    System.out.println("The procedure " + procedure + " returned: "+ result);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
